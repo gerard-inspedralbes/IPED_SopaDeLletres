43cf1622245dd6fecbcb5d762909991a7dab711d
isard <gtorrents@inspedralbes.cat>
2024-02-21 10:17:59 +0100

    Funcionalitat Carregar soper des d'un fitxer TXT

--- src/DataLayer.java
+++ src/DataLayer.java
@@ -3,16 +3,60 @@ import java.io.*;
 
 public class DataLayer {
 
-    static final String NOM_FITXER = "./sopes.bin";
+    static final String NOM_FITXER_BIN = "./sopes.bin";
+    static final String NOM_FITXER_TXT = "./sopes.txt";
+    static BufferedReader buffer;
+
+    static void guardarNovaSopaBytes(Sopa sopa) throws IOException {
+        if (sopa != null && !sopaExistent(sopa)) {
+            File arxiu = new File(NOM_FITXER_BIN);
+            FileOutputStream fos = new FileOutputStream(arxiu, true);
+            DataOutputStream dos = new DataOutputStream(fos);
+            escriureSopaBytes(sopa, dos);
+            fos.flush();
+            fos.close();
+        }
+    }
 
     static void guardarSopaBytes(Sopa sopa) throws IOException {
-        File arxiu = new File(NOM_FITXER);
-        FileOutputStream fos = new FileOutputStream(arxiu, false);
-        DataOutputStream dos = new DataOutputStream(fos);
-        escriureSopaBytes(sopa, dos);
-        fos.flush();
-        fos.close();
+        if (sopa != null && sopaExistent(sopa)) {
+            File arxiu = new File(NOM_FITXER_BIN);
+            FileOutputStream fos = new FileOutputStream(arxiu, true);
+            DataOutputStream dos = new DataOutputStream(fos);
+            escriureSopaBytes(sopa, dos);
+            fos.flush();
+            fos.close();
+        }
     }
+
+    private static boolean sopaExistent(Sopa nova_sopa) throws IOException {
+        DataInputStream dis = null;
+        try {
+            File arxiu = new File(NOM_FITXER_BIN);
+            if (!arxiu.exists()) {
+                return false;
+            }
+            FileInputStream fis = new FileInputStream(arxiu);
+            dis = new DataInputStream(fis);
+            while (true) {
+                Sopa sopa_existent = llegirSopaBytes(dis);
+                if (nova_sopa.getCadenaSopa().equals(sopa_existent.getCadenaSopa())) {
+                    //Hem torbat una sopa igual
+
+                    return true;
+                }
+            }
+        } catch (EOFException e) {
+            //hem arribat al final del fitxer sense trobar la sopa
+            return false;
+        } finally {
+            if (dis != null) {
+                //tanquem el fitxer
+                dis.close();
+            }
+        }
+    }
+
     private static void escriureSopaBytes(Sopa sopa, DataOutputStream dos) throws IOException {
         dos.writeUTF(sopa.getCadenaSopa());
         for (int i = 0; i < sopa.trobat.length; i++) {
@@ -21,13 +65,8 @@ public class DataLayer {
             }
         }
     }
-    static Sopa llegirSopaBytes() throws IOException {
-        File arxiu = new File(NOM_FITXER);
-        if (!arxiu.exists()){
-            return null;
-        }
-        FileInputStream fis = new FileInputStream(arxiu);
-        DataInputStream dis = new DataInputStream(fis);
+
+    static Sopa llegirSopaBytes(DataInputStream dis) throws IOException {
         Sopa s = new Sopa();
         s.setSopa(dis.readUTF());
         s.trobat = new boolean[10][10];
@@ -40,4 +79,39 @@ public class DataLayer {
     }
 
 
+    public static String llegirCadenaTXT() throws IOException {
+        if (buffer == null) {
+            openTXTFile();
+        }
+        String cadena = buffer.readLine();
+        if (cadena == null || cadena == "") {
+            closeTXTFile();
+        }
+        return cadena;
+    }
+
+    public static void openTXTFile() throws IOException {
+        File arxiu = new File(NOM_FITXER_TXT);
+        FileReader reader = new FileReader(arxiu);
+        buffer = new BufferedReader(reader);
+    }
+
+    public static void closeTXTFile() throws IOException {
+        buffer.close();
+    }
+
+    public static Sopa getSopa(int i) {
+        DataInputStream dis = null;
+        try {
+            File arxiu = new File(NOM_FITXER_BIN);
+            if (!arxiu.exists()) {
+                return null;
+            }
+            FileInputStream fis = new FileInputStream(arxiu);
+            dis = new DataInputStream(fis);
+            return llegirSopaBytes(dis);
+        } catch (IOException e) {
+            return null;
+        }
+    }
 }
--- src/SopaDeLletres.java
+++ src/SopaDeLletres.java
@@ -3,52 +3,91 @@ import Utils.Utils;
 public class SopaDeLletres {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_RESET = "\u001B[0m";
-        public static void main(String[] args) {
-        try {
-            int trobades = 0;
-            System.out.println("Benvinguts a la sopa de lletres");
-            System.out.println("");
-            Sopa s = DataLayer.llegirSopaBytes();
-            if (s == null)
-            {
-                String cadena = Utils.LlegirString("Introdueix la cadena de 100 lletres:", 100, 100);
-                s = crearSopaDeLletres(cadena);
+
+    public static void main(String[] args) {
+        System.out.println("Benvinguts a la sopa de lletres");
+        boolean sortir = false;
+        while (!sortir)
+            switch (mostrarMenu()) {
+                case 1:
+                    carregarSopes();
+                    break;
+                case 2:
+                    jugar();
+                case 0:
+                    sortir = true;
+                    System.out.println("Adeu!");
             }
+    }
+
+    private static void carregarSopes() {
+        try {
+            Sopa s = DataLayer.getSopa(1);
+            do {
+                s = crearSopaDeLletres(DataLayer.llegirCadenaTXT());
+                DataLayer.guardarNovaSopaBytes(s);
+            } while (s != null);
+        } catch (Exception e) {
+            //System.out.println("ERROR :" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void jugar() {
+        try {
+            Sopa s = DataLayer.getSopa(1);
+            if (s == null) {
+                System.out.println("No hi ha cap sopa creada, primer carregar un fitxer de sopes.");
+            }
+            int trobades = 0;
             while (trobades < 5) {
                 mostrarSopaDeLletres(s);
                 String paraula = Utils.LlegirString("Introdueix la paraula a buscar:", 1, 10);
-                if (buscarParaula(s,paraula)) {
+                if (buscarParaula(s, paraula)) {
                     System.out.println("Paraula trobada");
                     trobades++;
                 } else {
                     System.out.println("Paraula no trobada");
                 }
-                DataLayer.guardarSopaBytes(s);
-                s = DataLayer.llegirSopaBytes();
+                DataLayer.guardarNovaSopaBytes(s);
+                //s = DataLayer.llegirSopaBytes();
             }
             System.out.println("Has trobat totes les paraules");
-        }catch (Exception e){
-            System.out.println("ERROR :" + e.getMessage());
+        } catch (Exception e) {
+            //System.out.println("ERROR :" + e.getMessage());
+            e.printStackTrace();
         }
+
+    }
+
+    public static int mostrarMenu() {
+        String menu = """
+                *** MENÚ ***
+                1.- Carregar sopa des d'un fitxer
+                2.- Buscar paraules a una sopa
+                0.- Sortir
+                """;
+        return Utils.LlegirInt(menu, 0, 2);
     }
 
     /**
      * Busca una paraula a la sopa de lletres
+     *
      * @param paraula a buscar
      * @return true si la paraula ha estat trobada
      */
-    public static boolean buscarParaula(Sopa s,String paraula) {
+    public static boolean buscarParaula(Sopa s, String paraula) {
         for (int i = 0; i < s.sopa.length; i++) {
             for (int j = 0; j < s.sopa[i].length; j++) {
                 if (s.sopa[i][j] == paraula.charAt(0)) {
-                    int[][] coords = comprovarParaula(s,paraula, i, j, true);
+                    int[][] coords = comprovarParaula(s, paraula, i, j, true);
                     if (coords != null) {
-                        marcarParaulaTrobada(s,coords);
+                        marcarParaulaTrobada(s, coords);
                         return true;
                     } else {
-                        coords = comprovarParaula(s,paraula, i, j, false);
+                        coords = comprovarParaula(s, paraula, i, j, false);
                         if (coords != null) {
-                            marcarParaulaTrobada(s,coords);
+                            marcarParaulaTrobada(s, coords);
                             return true;
                         }
                     }
@@ -60,26 +99,28 @@ public class SopaDeLletres {
 
     /**
      * Marca la paraula com a trobada
+     *
      * @param coords coordenades de la paraula
      */
-    private static void marcarParaulaTrobada(Sopa s,int[][] coords) {
-    for (int i = 0; i < coords.length; i++) {
+    private static void marcarParaulaTrobada(Sopa s, int[][] coords) {
+        for (int i = 0; i < coords.length; i++) {
             s.trobat[coords[i][0]][coords[i][1]] = true;
         }
     }
 
     /**
      * Cerca la paraula a partir d'una posició inicial en una direcció
-     * @param paraula a cercar
-     * @param i cordenada inicial fila
-     * @param j oordenada inicial columna
+     *
+     * @param paraula  a cercar
+     * @param i        cordenada inicial fila
+     * @param j        oordenada inicial columna
      * @param vertical cert si cal cercar la paraula en vertical
      * @return llista de coordenades de la paraula si la paraula ha estat trobada, null si no ha estat trobada
      */
-    private static int[][] comprovarParaula(Sopa s,String paraula, int i, int j, boolean vertical) {
+    private static int[][] comprovarParaula(Sopa s, String paraula, int i, int j, boolean vertical) {
         int[][] coords = new int[paraula.length()][2];
         int k = 0;
-        while (k < paraula.length() ) {
+        while (k < paraula.length()) {
             if (i < s.sopa.length && j < s.sopa[i].length && s.sopa[i][j] == paraula.charAt(k)) {
                 coords[k][0] = i;
                 coords[k][1] = j;
@@ -104,7 +145,7 @@ public class SopaDeLletres {
         System.out.println("Sopa de lletres:");
         for (int i = 0; i < s.sopa.length; i++) {
             for (int j = 0; j < s.sopa[i].length; j++) {
-                pintarLletra(s.sopa[i][j], s.trobat[i][j],ANSI_RED);
+                pintarLletra(s.sopa[i][j], s.trobat[i][j], ANSI_RED);
                 System.out.print(" ");
             }
             System.out.println("");
@@ -113,8 +154,9 @@ public class SopaDeLletres {
 
     /**
      * Pinta una lletra per consola
-     * @param c lletra a pintar
-     * @param b si la lletra ha estat trobada
+     *
+     * @param c     lletra a pintar
+     * @param b     si la lletra ha estat trobada
      * @param color color de la lletra
      */
     private static void pintarLletra(char c, boolean b, String color) {
@@ -127,10 +169,14 @@ public class SopaDeLletres {
 
     /**
      * Crea una sopa de lletres a partir d'una cadena de mínim 100 lletres
+     *
      * @param cadena amb el condingut de la sopa de lletres
-     * @return sopa de lletres de 10x10
+     * @return sopa de lletres de 10x10 o null si no s'ha pogut construir la sopa amb la cadena rebuda
      */
     public static Sopa crearSopaDeLletres(String cadena) {
+        if(cadena == null || cadena =="" || cadena.length()<100){
+            return null;
+        }
         char[][] s = new char[10][10];
         crearSopaDeLletresRecursiu(s, cadena, 0, 0);
         Sopa sopa = new Sopa();
